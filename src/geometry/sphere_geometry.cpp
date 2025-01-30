939ee65aef466907101c1c048d1a14814a82f054
skalante1 <skalante1@FST-O-I-213-13>
2025-01-30 17:11:17 +0100

    commit 3

--- src/geometry/sphere_geometry.cpp
+++ src/geometry/sphere_geometry.cpp
@@ -1,15 +1,38 @@
 #include "sphere_geometry.hpp"
 
+
 namespace RT_ISICG
 {
 	bool SphereGeometry::intersect( const Ray & p_ray, float & p_t1, float & p_t2 ) const
 	{
+
 		/// TODO !
+		Vec3f oc = p_ray.getOrigin() - _center; 
+		float a	 = glm::dot( p_ray.getDirection(), p_ray.getDirection() ); 
+		float b	 = 2.0f * glm::dot( oc, p_ray.getDirection() );			 
+		float c	 = glm::dot( oc,oc ) - _radius * _radius;			
+		
 
-		p_t1 = -1.f;
-		p_t2 = -1.f;
+		float discriminant = b * b - 4 * a * c;
 
-		return false;
+		if ( discriminant < 0 )
+		{
+			
+			return false;
+		}
+		else
+		{
+			discriminant = std::sqrt( discriminant );
+			p_t1		 = ( -b - discriminant ) / ( 2.0f * a );
+			p_t2		 = ( -b + discriminant ) / ( 2.0f * a );
+
+			if ( p_t1 > p_t2 ) { std::swap( p_t1, p_t2 ); }
+
+			p_t1 = -1.f;
+			p_t2 = -1.f;
+
+			return true;
+		}
 	}
 
 } // namespace RT_ISICG
